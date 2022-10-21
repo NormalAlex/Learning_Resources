3d2920550c4e27beecf5e6103e9a29c062c829d6
MALAY SHAH <107268610+MALAY-21@users.noreply.github.com>
2022-10-21 12:00:34 +0530

    Update 230_A_Dragons.c
    Updated the C solution and shortened the code while using if-else and for loop

--- Codeforces_Problem/230_A_Dragons.c
+++ Codeforces_Problem/230_A_Dragons.c
@@ -1,45 +1,25 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
-#include <limits.h>
-
-int ascending (const void * a, const void * b) {
-return ( *(int*)a - *(int*)b );
-}
-int descending (const void * a, const void * b) {
-return ( *(int*)b - *(int*)a );
-}
-
-int main(){
-    long long s,n, b,c,d=0;
-    scanf("%lld%lld", &s,&n);
-    int arr[n][2];
-    for(int i=0;i<n;i++){
-        scanf("%lld%lld", &arr[i][0],&arr[i][1]);
-    }
-    for(int i=0;i<n;i++){
-        b=LONG_MAX;
-        for(int j=0;j<n;j++){
-            if(arr[j][0]<b){
-                b=arr[j][0];
-                c=j;
-            }
-        }
-        if(b>=s){
-            printf("NO");
-            break;
-        }
-        else{
-            d++;
-            arr[c][0]=INT_MAX;
-            s+=arr[c][1];
-        }
-        if(d==(n)){
-            printf("YES");
-            break;
-        }
-    }
-    
-    return 0;
+#include<stdio.h>
+int main()
+{
+	int n,s,x[1000],y[1000],i;
+	scanf("%d %d",&s,&n);
+	for(i=0;i<n;i++)scanf("%d %d",&x[i],&y[i]);
+	for(i=0;i<n;i++)
+	{
+		if(s>x[i]&&x[i]!=0)
+		{
+			s+=y[i];
+			x[i]=0;
+			i=-1;
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		if(x[i]!=0)
+		{
+			printf("NO");
+			return 0;
+		}
+	}
+	printf("YES");
 }
