9ddee63345cdf8fe6faa2f0fde5e7019a12107be
NormalAlex <152516808+NormalAlex@users.noreply.github.com>
2023-12-14 13:20:55 -0500

    Update Calculator.java
    Changed from integer math to double math -- better to emulate a calculator
    Changed the switch statement to arrows to make the code more easily readable
    Made simple comments

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
- 
+
 /*
- * Write a Calculator program which performs Addition, subtraction, multiplication and division of two numbers in java.
+ * Write a CalculatorProgramUsingSwitchStatementExample program which performs Addition, subtraction, multiplication and division of two numbers in java.
  */
  
 /** JavaMadeSoEasy.com */
 public class CalculatorProgramUsingSwitchStatementExample {
     public static void main(String[] args) {
- 
+           // Obtaining user input
            Scanner scanner = new Scanner(System.in);
  
            System.out.print("Enter number 1 : ");
-           int num1 = scanner.nextInt();
+           double num1 = scanner.nextDouble();
            
            System.out.print("Enter number 2 : ");
-           int num2 = scanner.nextInt();
+           double num2 = scanner.nextDouble();
            
            System.out.print("Enter > + for Addition, - for subtract, * for multiplication and / for division : ");
            String operation = scanner.next();
@@ -23,25 +23,15 @@ public class CalculatorProgramUsingSwitchStatementExample {
             * Perform calculator operations
             */
            switch (operation) {
-                  case "+":
-                        System.out.println("Add result > " + num1 + " + "+ num2 +" = " + (num1+num2));
-                        break;
+                  case "+" -> System.out.println("Add result > " + num1 + " + "+ num2 +" = " + (num1+num2)); // addition
     
-                  case "-":
-                        System.out.println("Subtract result > " + num1 + " - "+ num2 +" = " + (num1-num2));
-                        break;
+                  case "-" -> System.out.println("Subtract result > " + num1 + " - "+ num2 +" = " + (num1-num2)); // subtraction
  
-                  case "*":
-                        System.out.println("Multiply result > " + num1 + " * "+ num2 +" = " + (num1*num2));
-                        break;
+                  case "*" -> System.out.println("Multiply result > " + num1 + " * "+ num2 +" = " + (num1*num2)); // multiplication
                         
-                  case "/":
-                        System.out.println("Division result > " + num1 + " / "+ num2 +" = " + (num1/num2));
-                        break;
+                  case "/" -> System.out.println("Division result > " + num1 + " / "+ num2 +" = " + (num1/num2)); // division
                         
-                  default : //optional
-                        System.out.println("Invalid Grade");
-                        break; //optional
+                  default -> System.out.println("Invalid Grade"); //optional (case for invalid input)
            }
     }
 }
